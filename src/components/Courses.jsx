7544c1deabb8dcb1ca7cfb826233a21836ecd092
Irvin Fiz <irvin.fiz.dev@gmail.com>
2020-03-21 02:12:11 -0600

    resolvers

---
+++ src/components/Courses.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './courses.css'
+
+
+const Courses = () =>{
+    return (
+        <div className='courses'>
+            <div className='superior'>
+                superior
+            </div>
+            <div className='inferior'>
+                inferior
+            </div>
+        </div>
+    );
+};
+
+export default Courses;
