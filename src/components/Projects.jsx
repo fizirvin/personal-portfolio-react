e3846be88c7476df001cac7be1569f7b5b446ca6
Irvin Fiz <irvin.fiz.dev@gmail.com>
2020-06-25 04:29:46 -0500

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header'
+import '../App.css'
 
 const Projects = () =>{
     return (
@@ -7,76 +8,113 @@ const Projects = () =>{
             <Header></Header>
             <div className='header_section_title'>Projects</div>
             <div className='plus_content'>
+            <div className='projects_content'>
                 
-                <table className='skills_table'>
-                    <tbody>
+                <table className='projects_table'>
+                    
+                    <tbody className='projects_body'>
                     <tr>
-                            <td className='description_experience' colSpan='2'><p className='job_paragraph'><b>Project:</b> Injection Production Web App</p></td>
+                            <td className='project-table-row row-start'><b>Injection Production Web App</b></td>
                             
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'><a href="https://injection-app.irvinfiz.now.sh/">Injection Production Demo Deploy</a> </td> 
+                            <td className='project-table-row'><a href="https://injection-app.irvinfiz.now.sh/">Injection Prod Demo Deploy</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'><a href="https://github.com/fizirvin/injection-react">Client repo (github.com/fizirvin)</a></td> 
+                            <td className='project-table-row'><a href="https://github.com/fizirvin/injection-react">Client Repository</a></td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'><a href="https://github.com/fizirvin/injection-server">Backend repo (github.com/fizirvin)</a> </td> 
+                            <td className='project-table-row row-end'><a href="https://github.com/fizirvin/injection-server">Backend Repository</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience' colSpan='2'><p className='job_paragraph'><b>Project:</b> Injection Labels Web App</p></td>
+                            <td className='row-separation'>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                   
+                    <tbody className='projects_body'>
+                    <tr>
+                            <td className='project-table-row row-start'><b> Injection Labels Web App</b></td>
                             
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'><a href="https://injection-labels.irvinfiz.now.sh/">Labels Demo Deploy</a> </td> 
+                            <td className='project-table-row'><a href="https://injection-labels.irvinfiz.now.sh/">Labels Demo Deploy</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'><a href="https://github.com/fizirvin/injection-labels">Client repo (github.com/fizirvin)</a></td> 
+                            <td className='project-table-row'><a href="https://github.com/fizirvin/injection-labels">Client Repository</a></td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'><a href="https://github.com/fizirvin/injection-labels-server">Backend repo (github.com/fizirvin)</a> </td> 
+                            <td className='project-table-row row-end'><a href="https://github.com/fizirvin/injection-labels-server">Backend Repository</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience' colSpan='2'><p className='job_paragraph'><b>Project:</b> HR Web App</p></td>
+                            <td className='row-separation'>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody className='projects_body'>
+                    <tr>
+                            <td className='project-table-row row-start'><b>HR Web App</b></td>
                             
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Demo Deploy: <a href="https://hr-app.irvinfiz.now.sh/">https://app.irvinfiz.now.sh/</a> </td> 
+                            <td className='project-table-row'><a href="https://hr-app.irvinfiz.now.sh/">HR Demo Deploy</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Client repo: <a href="https://github.com/fizirvin/hr-app">https://github.com/fizirvin/</a></td> 
+                            <td className='project-table-row'><a href="https://github.com/fizirvin/hr-app">Client Repository</a></td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Backend repo: <a href="https://github.com/fizirvin/hr-app-server">https://github.com/fizirvin/</a> </td> 
+                            <td className='project-table-row row-end'><a href="https://github.com/fizirvin/hr-app-server">Backend Repository</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience' colSpan='2'><p className='job_paragraph'><b>Project:</b> Utility Cars Web App</p></td>
+                            <td className='row-separation'>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody className='projects_body'>
+                    <tr>
+                            <td className='project-table-row row-start'><b>Utility Cars Web App</b></td>
                             
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Demo Deploy: <a href="https://hr-cars-app.irvinfiz.now.sh/">https://app.irvinfiz.now.sh/</a> </td> 
+                            <td className='project-table-row'><a href="https://hr-cars-app.irvinfiz.now.sh/">Utility Cars Demo Deploy</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Client repo: <a href="https://github.com/fizirvin/hr-cars-app">https://github.com/fizirvin/</a></td> 
+                            <td className='project-table-row'><a href="https://github.com/fizirvin/hr-cars-app">Client Repository</a></td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Backend repo: <a href="https://github.com/fizirvin/hr-cars-server">https://github.com/fizirvin/</a> </td> 
+                            <td className='project-table-row row-end'><a href="https://github.com/fizirvin/hr-cars-server">Backend Repository</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience' colSpan='2'><p className='job_paragraph'><b>Project:</b> Warehouse Stock Web App</p></td>
+                            <td className='row-separation'>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody className='projects_body'>
+                    <tr>
+                            <td className='project-table-row row-start'><b>Warehouse Stock Web App</b></td>
                             
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Demo Deploy: <a href="https://warehouse-stock-app.irvinfiz.now.sh/">https://app.irvinfiz.now.sh/</a> </td> 
+                            <td className='project-table-row'><a href="https://warehouse-stock-app.irvinfiz.now.sh/">Warehouse Stock Demo Deploy</a> </td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Client repo: <a href="https://github.com/fizirvin/warehouse-stock-app">https://github.com/fizirvin/</a></td> 
+                            <td className='project-table-row'><a href="https://github.com/fizirvin/warehouse-stock-app">Client Repository</a></td> 
                         </tr>
                         <tr>
-                            <td className='description_experience project-row' colSpan='2'>Backend repo: <a href="https://github.com/fizirvin/warehouse-stock-server">https://github.com/fizirvin/</a> </td> 
+                            <td className='project-table-row row-end'><a href="https://github.com/fizirvin/warehouse-stock-server">Backend Repository</a> </td> 
+                        </tr>
+                        <tr>
+                            <td className='row-separation'>
+
+                            </td>
                         </tr>
                     </tbody>
                 </table>
+            </div>
             </div>
         </div>
     );
